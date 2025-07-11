4c18ae20f8b9e974a07f9c1df1b659425477f402
randydarthur <57815680+randydarthur@users.noreply.github.com>
2025-07-11 14:38:31 -0400

    Update streamlit_app.py
    address tabs vs spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ ingredients_list = st.multiselect (
 
 if ingredients_list:
     ingredients_string = ''
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
