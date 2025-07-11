986d47e8a2fba3c2a10d4bd6e5b44eeee2faa000
randydarthur <57815680+randydarthur@users.noreply.github.com>
2025-07-11 14:14:57 -0400

    Update streamlit_app.py
    Properly format the display

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if time_to_insert:
 # new section to import smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
